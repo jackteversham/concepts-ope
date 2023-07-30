2bc4c49025a436fe7c459b4c595641227fb56b3c
Jack Teversham <jacktevers@gmail.com>
2023-07-30 21:00:42 +0100

    concept experiments complete for new express gridworld

--- models/policy.py
+++ models/policy.py
@@ -2,9 +2,10 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 class PolicyModel():
-    def __init__(self, input_size, num_actions) -> None:
+    def __init__(self, input_size, num_actions, loss) -> None:
         self.input_size = input_size
         self.output_size = num_actions
+        self.loss = loss #categorical_crossentropy or mse
     
     def build(self):
         model = Sequential()
@@ -12,5 +13,7 @@ class PolicyModel():
         model.add(Dense(128, activation="relu"))
         model.add(Dense(self.output_size, activation="softmax"))
 
-        model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+        metric = "mse" if self.loss == "mse" else "categorical_accuracy"
+
+        model.compile(optimizer='adam',loss=self.loss, metrics=[metric])
         return model
